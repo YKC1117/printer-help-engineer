f00ea4e4d6ea1cceb5b37237261fa5107c8bed2a
可樂 <108776285+YKC1117@users.noreply.github.com>
2026-09-07 14:49:54 +0800

    release: bump remote command center to v4.2

--- app-version.js
+++ app-version.js
@@ -2,8 +2,8 @@
 
 // 全站唯一版本來源。版本號、更新時間與時區只在此維護；其他模組一律讀取 window.APP_BUILD。
 window.APP_BUILD=Object.freeze({
-  version:'v4.1',
-  updated:'2026/09/07 14:28',
+  version:'v4.2',
+  updated:'2026/09/07 14:47',
   timezone:'Asia/Taipei',
   schema:1
 });

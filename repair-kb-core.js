16d9fca2bc59ddc4ba1e48fb65bdd8fe6de6b665
可樂 <108776285+YKC1117@users.noreply.github.com>
2026-09-07 10:24:07 +0800

    Expand repair knowledge base source index

--- repair-kb-core.js
+++ repair-kb-core.js
@@ -5,6 +5,10 @@ window.KB_SOURCES={
   zebra_zt600_tw:{label:'Zebra ZT610/ZT620 使用者指南（繁中）',level:'A｜原廠',url:'https://docs.zebra.com/tw/tw/printers/industrial/zt610-zt620-industrial-printer-with-color-touch-di/-zt610-zt620-.html'},
   zebra_zt400:{label:'Zebra ZT411/ZT421 Industrial Printer User Guide',level:'A｜原廠',url:'https://docs.zebra.com/us/en/printers/industrial/zt411-zt421-industrial-printer-user-guide/c-zt411-zt421-industrial-printer-user-guide.html'},
   zebra_zt231:{label:'Zebra ZT231 Industrial Printer User Guide',level:'A｜原廠',url:'https://docs.zebra.com/us/en/printers/industrial/zt231-industrial-printer-user-guide.html'},
+  zebra_zt510:{label:'Zebra ZT510 Industrial Printer User Guide',level:'A｜原廠',url:'https://docs.zebra.com/us/en/printers/industrial/zt510-ug/c-zt510-ug-zt510-industrial-printer-user-guide.html'},
+  zebra_zt230:{label:'Zebra ZT230 Support / Calibration Resources',level:'A｜原廠舊機支援',url:'https://www.zebra.com/us/en/support-downloads/printers/industrial/zt230.html'},
+  zebra_zd421:{label:'Zebra ZD421 Series Support / User Guide',level:'A｜原廠',url:'https://www.zebra.com/us/en/support-downloads/printers/desktop/zd421.html'},
+  zebra_zd421_cal:{label:'Zebra ZD421/ZD621 SmartCal & Manual Calibration',level:'A｜原廠',url:'https://docs.zebra.com/us/en/printers/desktop/zd421-and-zd621-desktop-printers-user-guide/setup/running-a-smartcal-media-calibration.html'},
   zebra_jg:{label:'Zebra ZPL ~JG Sensor Profile',level:'A｜原廠',url:'https://docs.zebra.com/content/tcm/us/en/printers/software/zpl-pg/zpl-commands/~jg.html'},
   zebra_cal:{label:'Zebra ZPL Calibration / Media Feed Commands',level:'A｜原廠',url:'https://docs.zebra.com/content/tcm/us/en/printers/software/zpl-pg/advanced-techniques/calibration-and-media-feed-commands.html'},
   zebra_xi4:{label:'Zebra Xi4/RXi4 User Guide',level:'A｜原廠舊機文件',url:'https://www.zebra.com/content/dam/support-dam/en/documentation/unrestricted/guide/product/xi4-ug-en.pdf'},
@@ -13,8 +17,14 @@ window.KB_SOURCES={
   tsc_th_product:{label:'TSC TH Series 4-inch Product / FAQ',level:'A｜原廠',url:'https://usca.tscprinters.com/en/products/th-series-4-inch-durable-desktop-printers'},
   tsc_mh241:{label:'TSC MH241 Series 使用者手冊',level:'A｜原廠',url:'https://fs.tscprinters.com/system/files/31-1600004-00_mh241_user-manual_tc_a.pdf'},
   tsc_mh_product:{label:'TSC MH241 Series Product / FAQ',level:'A｜原廠',url:'https://usca.tscprinters.com/en/products/mh241-series-4-inch-performance-industrial-printers'},
+  tsc_te:{label:'TSC TE Series 4-inch Product / FAQ',level:'A｜原廠',url:'https://usca.tscprinters.com/en/products/te-series-4-inch-performance-desktop-printers'},
+  tsc_ttp:{label:'TSC TTP Series 4-inch Product / Resources',level:'A｜原廠',url:'https://usca.tscprinters.com/en/products/ttp-series-4-inch-performance-desktop-printers'},
   argox_p4:{label:'Argox P4 Series User Manual V1.9',level:'A｜原廠',url:'https://www.argox.com/docfile/usermanual/P4-Series-User-Manual_V1.9_EN.pdf'},
-  argox_p4_product:{label:'Argox P4-650 產品頁',level:'A｜原廠',url:'https://www.argox.com/tw/products-detail/p4_650/'}
+  argox_p4_product:{label:'Argox P4-650 產品頁',level:'A｜原廠',url:'https://www.argox.com/tw/products-detail/p4_650/'},
+  argox_os214:{label:'Argox OS-214EX Pro / OS-200 Pro User Manual',level:'A｜原廠',url:'https://www.argox.com/docfile/usermanual/OS-214EX_Pro_OS-200_Pro_Users_Manual_V1.0_EN.pdf'},
+  argox_cp:{label:'Argox CP-EX / CX Series User Manual V1.9',level:'A｜原廠',url:'https://www.argox.com/docfile/usermanual/CP-EX-Series_CX-Series-User-Manual_V1.9_EN.pdf'},
+  sato_clnx_cal:{label:'SATO CL4NX Plus Sensor Calibration',level:'A｜原廠',url:'https://www.manual.sato-global.com/printer/clnxplus/main/main_GUID-53F86AA6-8E10-4242-816F-EAEFDE1B67E3.html'},
+  sato_clnx_quality:{label:'SATO CL4NX Plus Print Quality Troubleshooting',level:'A｜原廠',url:'https://www.manual.sato-global.com/printer/clnxplus/troubleshooting/troubleshooting_GUID-42359747-2FCD-4099-AD0F-EC78944595E2.html'}
 };
 window.REPAIR_KB=[];
 window.addRepairKB=function(x){window.REPAIR_KB.push(x)};

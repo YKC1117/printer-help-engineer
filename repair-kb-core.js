b08f9effd5e0073c78d0d618ab1bd78734e83ff5
可樂 <108776285+YKC1117@users.noreply.github.com>
2026-09-07 09:41:13 +0800

    v3.2 add repair knowledge base sources

---
+++ repair-kb-core.js
@@ -0,0 +1,18 @@
+'use strict';
+
+window.KB_SOURCES={
+  zebra_zt600:{label:'Zebra ZT610/ZT620 User Guide',level:'A｜原廠',url:'https://docs.zebra.com/us/en/printers/industrial/zt610-zt620-industrial-printer-with-color-touch-display-user-guide.html'},
+  zebra_zt600_tw:{label:'Zebra ZT610/ZT620 使用者指南（繁中）',level:'A｜原廠',url:'https://docs.zebra.com/tw/tw/printers/industrial/zt610-zt620-industrial-printer-with-color-touch-di/-zt610-zt620-.html'},
+  zebra_jg:{label:'Zebra ZPL ~JG Sensor Profile',level:'A｜原廠',url:'https://docs.zebra.com/content/tcm/us/en/printers/software/zpl-pg/zpl-commands/~jg.html'},
+  zebra_cal:{label:'Zebra ZPL Calibration / Media Feed Commands',level:'A｜原廠',url:'https://docs.zebra.com/content/tcm/us/en/printers/software/zpl-pg/advanced-techniques/calibration-and-media-feed-commands.html'},
+  zebra_xi4:{label:'Zebra Xi4/RXi4 User Guide',level:'A｜原廠舊機文件',url:'https://www.zebra.com/content/dam/support-dam/en/documentation/unrestricted/guide/product/xi4-ug-en.pdf'},
+  zebra_110:{label:'Zebra 110Xi4 Support',level:'A｜原廠舊機支援',url:'https://www.zebra.com/gb/en/support-downloads/printers/industrial/110xi4.html'},
+  tsc_th240:{label:'TSC TH240/DH240 Series User Manual',level:'A｜原廠',url:'https://fs.tscprinters.com/system/files/31-2250007-00_th240_dh240_user_manual_en_c_0.pdf'},
+  tsc_th_product:{label:'TSC TH Series 4-inch Product / FAQ',level:'A｜原廠',url:'https://usca.tscprinters.com/en/products/th-series-4-inch-durable-desktop-printers'},
+  tsc_mh241:{label:'TSC MH241 Series 使用者手冊',level:'A｜原廠',url:'https://fs.tscprinters.com/system/files/31-1600004-00_mh241_user-manual_tc_a.pdf'},
+  tsc_mh_product:{label:'TSC MH241 Series Product / FAQ',level:'A｜原廠',url:'https://usca.tscprinters.com/en/products/mh241-series-4-inch-performance-industrial-printers'},
+  argox_p4:{label:'Argox P4 Series User Manual V1.9',level:'A｜原廠',url:'https://www.argox.com/docfile/usermanual/P4-Series-User-Manual_V1.9_EN.pdf'},
+  argox_p4_product:{label:'Argox P4-650 產品頁',level:'A｜原廠',url:'https://www.argox.com/tw/products-detail/p4_650/'}
+};
+window.REPAIR_KB=[];
+window.addRepairKB=function(x){window.REPAIR_KB.push(x)};
